d5ed7c75fa4264e323b10aa60c07c1ec44c2765d
Stefano Frati <frati.stefano97@gmail.com>
2020-05-10 09:23:08 +0200

    Comments

--- RandomPasswordGenerator.c
+++ RandomPasswordGenerator.c
@@ -1,37 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#define LEN 12
+#define LEN 12 //password's length is defined here
 
+//returns a random printable character except SPACE and DELETE 
 char randomChar(){
-  int r = rand() % 94 + 33;
-  return (char) r;
+	int r = rand() % 94 + 33;
+	return (char) r;
 }
 
-
+//returns a random password that has to contain both symbols, uppercase letters, lower case letters and numbers 
 char* pswGen(char *psw){
-  int i, letter, capLetter, number, symbol;
-  char randChar;
-do{
+	int i, letter, capLetter, number, symbol;
+	char randChar;
+	
+	do{
 
-  letter = 0, capLetter = 0, number = 0, symbol = 0;
+		letter = 0, capLetter = 0, number = 0, symbol = 0;
 
-  for(i = 0; i<LEN; i++){
-    randChar = randomChar();
+		for(i = 0; i<LEN; i++){
+			randChar = randomChar();
 
-    if(randChar >= 97 & randChar <= 122) 
-      letter++;
-    else if (randChar >= 48 & randChar <= 57)
-      number++;
-    else if (randChar >= 65 & randChar <= 90)
-      capLetter++;
-    else symbol++;
-    
-     psw[i] = randChar;
-  }
-}while(letter == 0 | capLetter == 0 | number == 0 | symbol == 0);
+			if(randChar >= 97 & randChar <= 122) 
+				letter++;
+			else if (randChar >= 48 & randChar <= 57)
+				number++;
+			else if (randChar >= 65 & randChar <= 90)
+				capLetter++;
+			else symbol++;
+		
+			psw[i] = randChar;
+		}
+	}while(letter == 0 | capLetter == 0 | number == 0 | symbol == 0);
 
-  return psw;
+	return psw;
 }
 
 int main(void) {
